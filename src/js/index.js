98efbf271dbfbd25fe57774c85b9404781b14ebf
Robert <51803593+rotyvk7@users.noreply.github.com>
2022-07-18 23:31:55 -0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@
 
 const personagens = document.querySelectorAll('.personagem')
 
-//forEach seria 'para cada' e com isso PARA CADA personagem dessa lista, eu quero fazer alguma coisa
 personagens.forEach(personagem => {
   personagem.addEventListener('click', () => {
 
@@ -11,27 +10,16 @@ personagens.forEach(personagem => {
     const personagemSelecionado = document.querySelector('.selecionado')
     personagemSelecionado.classList.remove('selecionado')
     personagem.classList.add('selecionado')
-
-    /*
-
-  OBJETIVO2 - quando apssar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande
-
-  - alterar a imagem do jogador 1
-  - alerar o nome do jogador 1
-    */
-
-  
     
-  const imagemJogador1 = document.getElementById('personagem-jogador-1')
-  imagemJogador1.src = `./src/imagens/${idSelecionado}.JPG`
+    const imagemJogador1 = document.getElementById('personagem-jogador-1')
+    imagemJogador1.src = `./src/imagens/${idSelecionado}.png`
 
-  const nomeJogador1 = document.getElementById('nome-jogador-1')
-  const nomeSelecionado1 = personagem.getAttribute('data-name')
+    const nomeJogador1 = document.getElementById('nome-jogador-1')
+    const nomeSelecionado1 = personagem.getAttribute('data-name')
 
 
   nomeJogador1.innerHTML = nomeSelecionado1;
 
   })
-
 
 })
